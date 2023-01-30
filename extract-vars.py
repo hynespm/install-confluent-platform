ef62d5df35d56daa36edd67665b3a92cdf0a1d9c
Patrick Hynes <phynes@confluent.io>
2023-01-30 15:55:17 +0100

    #2 : Update of pipeline and approach

--- extract-vars.py
+++ extract-vars.py
@@ -3,4 +3,12 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-logging.info(os.environ['GITHUB_REF'])
+branch = "refs/heads/7.2.2"#os.environ['GITHUB_REF']
+branch_name = branch.split("/")
+branch_name_value = branch_name[len(branch_name)-1]
+major_version = branch_name_value.split(".")
+maj_ver = major_version[0] + "." + major_version[1]
+min_ver = maj_ver + major_version[2]
+logging.info(maj_ver)
+os.environ["CONFLUENT_MAJOR_VERSION"] = maj_ver
+os.environ["CONFLUENT_MINOR_VERSION"] = min_ver
